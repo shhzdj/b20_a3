bb25be74e687680646ea0dcd3ea9d57e44162b70
shhzdj <shahzadjalil1@gmail.com>
2022-04-05 00:51:09 -0400

    login and register
    You can login and register as a student or an instructor.

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ class users(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(20), unique=True, nullable = False)
-    firstName = db.Column(db.String(20), unique=True, nullable = False)
-    lastName = db.Column(db.String(20), unique=True, nullable = False)
+    firstName = db.Column(db.String(20), nullable = False)
+    lastName = db.Column(db.String(20), nullable = False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(20), nullable = False)
-    type = db.Column(db.String(20), unique=True, nullable = False)
+    type = db.Column(db.String(20), nullable = False)
     #notes = db.relationship('Notes', backref='author', lazy = True)
 
     def __repr__(self):
@@ -38,11 +38,17 @@ def register():
     else:
         username = request.form['Username']
         email = request.form['Email']
+        firstName = request.form['Firstname']
+        lastName = request.form['Lastname']
         hashed_password = bcrypt.generate_password_hash(request.form['Password']).decode('utf-8')
+        type = request.form['Option']
         reg_details = [
             username,
             email,
-            hashed_password
+            firstName,
+            lastName,
+            hashed_password,
+            type
         ]
         add_users(reg_details)
         return redirect(url_for('login'))
@@ -50,15 +56,12 @@ def register():
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        
-
         return render_template('login.html')
     else:
         username = request.form['Username']
         password= request.form['Password']
-        person = Person.query.filter_by(username = username).first()
-        if not person or not bcrypt.check_password_hash(person.password,password):
-
+        user = users.query.filter_by(username = username).first()
+        if not user or not bcrypt.check_password_hash(user.password,password):
             flash('Please check your login details', 'error')
             return render_template('login.html')
         else:
@@ -68,8 +71,8 @@ def login():
 
 
 def add_users(reg_details):
-    person = Person(username = reg_details[0], email = reg_details[1], password = reg_details[2])
-    db.session.add(person)
+    user = users(username = reg_details[0], email = reg_details[1], firstName = reg_details[2], lastName=reg_details[3],  password = reg_details[4], type = reg_details[5])
+    db.session.add(user)
     db.session.commit()
 if __name__ == '__main__':
     app.run(debug=True)
